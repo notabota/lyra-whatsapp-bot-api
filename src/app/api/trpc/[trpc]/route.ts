f4ea99555e63b9361912fc1250f15a6ca5067876
notabota <nnv2205owo@gmail.com>
2024-12-17 15:35:28 +0700

    initiate

---
+++ src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,51 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { type NextRequest } from "next/server";
+
+import { env } from "~/env";
+import { appRouter } from "~/server/api/root";
+import { createTRPCContext } from "~/server/api/trpc";
+
+import { generateOpenApiDocument } from 'trpc-to-openapi';
+import { createOpenApiFetchHandler } from 'trpc-to-openapi';
+
+/**
+ * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
+ * handling a HTTP request (e.g. when you make requests from Client Components).
+ */
+const createContext = async (req: NextRequest) => {
+  return createTRPCContext({
+    headers: req.headers,
+  });
+};
+
+const handler = (req: NextRequest) =>
+  createOpenApiFetchHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: () => createContext(req),
+    onError:
+      env.NODE_ENV === "development"
+        ? ({ path, error }) => {
+            console.error(
+              `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            );
+          }
+        : undefined,
+  });
+
+export const openApiDocument = generateOpenApiDocument(appRouter, {
+  title: 'tRPC OpenAPI',
+  version: '1.0.0',
+  baseUrl: 'http://localhost:3000/api/trpc',
+});
+
+export {
+  handler as GET,
+  handler as POST,
+  handler as PUT,
+  handler as PATCH,
+  handler as DELETE,
+  handler as OPTIONS,
+  handler as HEAD,
+};
---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,59 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const postRouter = createTRPCRouter({
+  chats: publicProcedure
+    .meta({ openapi: { method: 'GET', path: '/hello' } })
+    .input(z.void())
+    .output(z.array(z.object({ id: z.string(), name: z.string().nullable() })))
+    .query(async ({ ctx }) => {
+      return await ctx.db.chats.findMany();
+    }),
+
+  messages: publicProcedure
+    .meta({ openapi: { method: 'GET', path: '/messages' } })
+    .input(z.void())
+    .output(z.array(z.object({
+      id: z.string(),
+      body: z.string().nullable(),
+      author: z.string().nullable(),
+      from: z.string().nullable(),
+      to: z.string().nullable(),
+      type: z.string().nullable(),
+      data: z.string().nullable(),
+      filename: z.string().nullable(),
+      mimetype: z.string().nullable(),
+      filesize: z.number().nullable(),
+      timestamp: z.number().nullable()
+    })))
+    .query(async ({ ctx }) => {
+      return await ctx.db.messages.findMany();
+    }),
+
+  contacts: publicProcedure
+    .meta({ openapi: { method: 'GET', path: '/contacts' } })
+    .input(z.void())
+    .output(z.array(z.object({
+      id: z.string(),
+      name: z.string().nullable(),
+      number: z.string().nullable(),
+      pushname: z.string().nullable(),
+      shortName: z.string().nullable()
+    })))
+    .query(async ({ ctx }) => {
+      return await ctx.db.contacts.findMany();
+    }),
+
+  contactChat: publicProcedure
+    .meta({ openapi: { method: 'GET', path: '/contact-chat' } })
+    .input(z.void())
+    .output(z.array(z.object({
+      id: z.number(),
+      contact_id: z.string().nullable(),
+      chat_id: z.string().nullable()
+    })))
+    .query(async ({ ctx }) => {
+      return await ctx.db.contact_chat.findMany();
+    }),
+});
